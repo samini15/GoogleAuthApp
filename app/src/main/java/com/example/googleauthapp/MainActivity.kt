f1c96ef1d23b0aedd87fa26efb3d412acad79008
Shayan <shayan.amini76@gmail.com>
2023-08-24 17:28:43 +0200

    Compose navigation basic setup

--- app/src/main/java/com/example/googleauthapp/MainActivity.kt
+++ app/src/main/java/com/example/googleauthapp/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.googleauthapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.googleauthapp.navigation.SetUpNavGraph
 import com.example.googleauthapp.ui.theme.GoogleAuthAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +12,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GoogleAuthAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                SetUpNavGraph(navController = navController)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GoogleAuthAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/googleauthapp/navigation/NavGraph.kt
@@ -0,0 +1,18 @@
+package com.example.googleauthapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+
+@Composable
+fun SetUpNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Login.route) {
+        composable(route = Screen.Login.route) {
+
+        }
+        composable(route = Screen.Profile.route) {
+
+        }
+    }
+}
