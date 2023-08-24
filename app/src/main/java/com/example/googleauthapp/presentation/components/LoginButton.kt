ff7bf129701ef500a9581c41368b5333ab43095e
Shayan <shayan.amini76@gmail.com>
2023-08-24 22:58:39 +0200

    Design of Login button component

---
+++ app/src/main/java/com/example/googleauthapp/presentation/components/LoginButton.kt
@@ -0,0 +1,99 @@
+package com.example.googleauthapp.presentation.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.googleauthapp.R
+import com.example.googleauthapp.ui.theme.LARGER_PADDING
+import com.example.googleauthapp.ui.theme.LARGE_PADDING
+import com.example.googleauthapp.ui.theme.MEDIUM_PADDING
+
+@Composable
+fun LoginButton(
+    modifier: Modifier = Modifier,
+    loadingState: Boolean = false,
+    primaryText: String = "Sign in with Google",
+    secondaryText: String = "Please Wait...",
+    icon: Int = R.drawable.ic_google_logo,
+    borderColor: Color = Color.LightGray,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    progressIndicatorColor: Color = Color.Blue,
+    onClick: () -> Unit
+) {
+    var buttonText by remember { mutableStateOf(primaryText) }
+
+    LaunchedEffect(key1 = loadingState) {
+        buttonText = if (loadingState) secondaryText else primaryText
+    }
+
+    Surface(
+        modifier = modifier
+            .clickable(enabled = !loadingState) {
+                onClick()
+            },
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = backgroundColor
+    ) {
+        Row(modifier = Modifier
+            .padding(LARGE_PADDING)
+            .animateContentSize(
+                animationSpec = tween(
+                    300,
+                    easing = LinearOutSlowInEasing
+                )
+            ),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(painter = painterResource(id = icon), contentDescription = "", tint = Color.Unspecified)
+
+            Spacer(modifier = Modifier.width(MEDIUM_PADDING))
+            
+            Text(text = buttonText)
+
+            if (loadingState) {
+                Spacer(modifier = Modifier.width(LARGER_PADDING))
+
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(LARGER_PADDING),
+                    strokeWidth = 2.dp,
+                    color = progressIndicatorColor
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LoginButtonPreview() {
+    LoginButton {
+
+    }
+}
---
+++ app/src/main/java/com/example/googleauthapp/ui/theme/Dimens.kt
@@ -0,0 +1,10 @@
+package com.example.googleauthapp.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val LARGEST_PADDING = 24.dp
+val LARGER_PADDING = 16.dp
+val LARGE_PADDING = 12.dp
+val MEDIUM_PADDING = 8.dp
+val SMALL_PADDING = 4.dp
+val TINY_PADDING = 2.dp
